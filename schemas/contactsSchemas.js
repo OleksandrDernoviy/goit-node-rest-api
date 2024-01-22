137498f9043e0e9bc36345ef231cc1662a6ff4c9
Oleksandr Dernoviy <130732150+Aex-Derny@users.noreply.github.com>
2024-01-22 14:36:50 +0200

    upEx

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -3,17 +3,23 @@ const Joi = require("joi");
 
 const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string().email()
+    .message("Invalid email format").required(),
+  phone: Joi.string()
+    .pattern(/^[\d() -]+$/)
+    .message("Phone must contain only numbers")
+    .required(),
 });
 
 const updateContactSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string(),
+  email: Joi.string().email().message("Invalid email format"),
   phone: Joi.string()
+    .pattern(/^[\d() -]+$/)
+    .message("Phone must contain only numbers"),
 })
   .min(1)
-  .message("Body must have at least one field")
+  .message("Body must have at least one field");
   
 
 module.exports = {
